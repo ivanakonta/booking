26e5bbe768bd3570dccfee79531247a14aafb275
Ivana Konta <ivana.konta99@gmail.com>
2025-02-22 18:44:26 +0100

    Kalendar fix

--- src/Form/ReservationFormType.php
+++ src/Form/ReservationFormType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ReservationFormType extends AbstractType
 {
@@ -22,8 +23,14 @@ class ReservationFormType extends AbstractType
             ->add('date', DateType::class, [
                 'widget' => 'single_text',
                 'html5' => true,
-                'attr' => ['class' => 'form-control'],
-                'data' => $options['data']->getDate() ?: new \DateTime(), // Use the entity's date or default to today
+                'attr' => ['class' => 'form-control', 'min' => (new \DateTime())->format('Y-m-d')],
+                'data' => $options['data']->getDate() ?: new \DateTime(),
+                'constraints' => [
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 'today',
+                        'message' => 'Datum ne može biti u prošlosti.',
+                    ]),
+                ],
             ])
             ->add('numberOfPersons')
             ->add('restaurant', EntityType::class, [
@@ -40,7 +47,8 @@ class ReservationFormType extends AbstractType
                     // Filter by selected restaurant
                     return $er->createQueryBuilder('v')
                         ->where('v.restaurant = :restaurant')
-                        ->setParameter('restaurant', $restaurant);
+                        ->setParameter('restaurant', $restaurant)
+                        ->orderBy('v.time', 'ASC'); // Sortiranje po vremenu
                 },
             ])
         ; 
